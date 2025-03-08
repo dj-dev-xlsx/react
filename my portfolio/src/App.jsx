98acdec10dd9beb79dc9be4cb93973b1d2b06256
dj-dev-xlsx <djbalila7@gmail.com>
2025-03-08 20:18:13 +0800

    testing

---
+++ my portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='p-5 bg-amber-950 rounded-b-xl'>
+        <h1 className="text-amber-200">Hello</h1>
+        <h1>Yoooo</h1>
+        <h1></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
